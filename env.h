a07d143f5671dac485e5d182113052725a4a3ce6
mqrb01 <71334913+mqrb01@users.noreply.github.com>
2023-01-14 12:40:41 -0300

    Update env.h

--- env.h
+++ env.h
@@ -1,1 +1,17 @@
+#ifndef __ENV_H__
+#define __ENV_H__
+#include "map.h"
 
+typeded struct{
+	Map *VARMAP;
+	int size;
+	int capacity; 
+}Env;
+
+Env *env_create();
+char *env_getVar(Env *env, char *key);
+void env_setVar(Env *env, char *key, char *value);
+Env *env_read(char *nameFile);
+void env_free(Env *env);
+
+#endif
