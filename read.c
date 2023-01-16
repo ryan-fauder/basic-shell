a4c9871b1d81e41b99b9e2bf69945c27f0346290
Ryan Auder <ryan.fauder@gmail.com>
2023-01-16 16:22:21 -0300

    refactor(read)

--- read.c
+++ read.c
@@ -6,16 +6,16 @@
 extern const int _COMMAND_SIZE;
 
 
-void tokenize (char *command, char separator, reader *t) {
+void tokenize (Reader *reader, char *string, char separator) {
   char *pt;
-  int separator_count = char_counter(command, separator);
+  int separator_count = char_counter(string, separator);
   int size_vector = separator_count + 1;
   printf("Tamanho vetor tokenized : %d\n", size_vector);
   char **vector = (char **) malloc(size_vector*sizeof(char*));
   for(int i = 0; i < size_vector; i++) {
     vector[i] = (char*) malloc(_COMMAND_SIZE*sizeof(char));
   }
-  pt = strtok (command, &separator);
+  pt = strtok (string, &separator);
   strcpy(vector[0], pt);
 
   int i = 1;
@@ -27,8 +27,8 @@ void tokenize (char *command, char separator, reader *t) {
     i++;
   }
 
-  t->tokens = vector;
-  t->length = i;
+  reader->tokens = vector;
+  reader->length = i;
 }
 
 char* read_line(char *input) {
--- read.h
+++ read.h
@@ -4,15 +4,18 @@
 typedef struct str_tokens {
     char ** tokens;
     int length;
-} reader;
+    int capacity;
+} Reader;
 
 void clear_input();
 int space_counter(char *);
 
-reader* createReader();
-void pushReader(reader *r);
-void popReader(reader *r);
-void tokenize (char *, char, reader *);
+Reader* reader_create();
+void reader_push(Reader *reader);
+void reader_pop(Reader *reader);
+void reader_realloc(Reader *reader, int new_capacity);
+char * reader_toString(Reader *reader);
+void tokenize (Reader *reader, char * string, char separator);
 char* read_line();
 
 #endif //__READ_H__
