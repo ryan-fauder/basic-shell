b3b5dc73aad42e733f836ff9fde424207651d560
Gabriel Oliveira <gabriel.oliveira@irancho.com.br>
2023-01-17 00:07:46 -0300

    fix conflicts

--- read.h
+++ read.h
@@ -14,7 +14,7 @@ reader* createReader(int initial_size);
 void pushReader(reader *r, char *str);
 void popReader(reader *r);
 void tokenize (char *, char, reader *);
-char* read_line(char *input);
+char* read_line();
 void freeReader(reader *r);
 void print_reader(reader *r);
 char* concatTokens(reader *r);
