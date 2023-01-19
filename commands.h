c758921ac704dd6a86f0ebb587726e106b974b42
mqrb01 <71334913+mqrb01@users.noreply.github.com>
2023-01-19 07:38:02 -0300

    Update commands.h

--- commands.h
+++ commands.h
@@ -2,10 +2,32 @@
 #define __COMMANDS_H__
 
 #include "env.h"
-
+/**
+*@brief Cria um painel com as opções possíveis de comandos
+*
+*/
 void command_ajuda();
+/**
+*@brief Mostrar todas as váriáveis existentes  
+*
+*@param env: Env*
+*/
 void command_amb_getAll(Env *env);
+/**
+*@brief Vai pegar a variável especificada dentro do pair 
+*
+*@paran env: Env*
+*@param nameVar: char*
+*/
 void command_amb_getVar(Env *env, char *nameVar);
+/**
+*@brief Vai atribuir o valor a variável adicionada
+*
+*@param env: Env*
+*@param nameVar: char*
+*@param valueVar: char*
+*/
+
 void command_amb_setVar(Env *env, char *nameVar, char *valueVar);
 void command_externCommand(Env *env, char *command, char **argv);
 void command_changeDir(Env * env, char *path, char *dta);
