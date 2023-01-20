9fa137be1d1ecfb1f899494a9e50f25e1fcbe4ff
mqrb01 <71334913+mqrb01@users.noreply.github.com>
2023-01-19 22:25:40 -0300

    Update commands.h

--- commands.h
+++ commands.h
@@ -29,10 +29,37 @@ void command_amb_getVar(Env *env, char *nameVar);
 */
 
 void command_amb_setVar(Env *env, char *nameVar, char *valueVar);
+/**
+*@brief Executa um comando externo 
+*
+*@param env: Env*
+*@param command: char*
+*@param argvr: char**
+*/
 void command_externCommand(Env *env, char *command, char **argv);
+/**
+*@brief Vai mudar o diretório no terminal 
+*
+*@param env: Env*
+*@param path: char*
+*@param dta: char*
+*/
+
 void command_changeDir(Env * env, char *path, char *dta);
+/**
+*@brief Vai limpar a shell
+*
+*/
 void command_limpa();
+/**
+*@brief Função que mostra o histórico  
+*
+*/
 void command_print_history();
+/**
+*@brief Sai da shell 
+*
+*/
 void command_sair();
 
 #endif // __COMMANDS_H__
